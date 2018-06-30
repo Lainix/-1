bf5eb99ddc61965a9bd6648bfcd8669ed57f3079
Lainix <linkelin8@gmail.com>
2018-06-30 08:54:11 -0500

    Se agrega proyecto plus 1

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+  $(".publish").click(getText);
+  var apikey = "xBVHjDnzrYiNAH4i7AApRbO1kzO4QBjw";
+
+  $('#search-button').click(function () {
+    var search = $('#search-text').val(); //texto con el que se va a hacer la busqueda
+    //$('#characters-container').html(''); //LIMPIAR LA CAJA DESPUES DEL CLICK
+    //$('#characters-container').innerHTML = '';
+
+    $('#search-text').val(''); // limpia lo escrito en el input
+    console.log(search);
+
+    $.ajax({
+      url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + apikey + "&keyword=" + search,
+      success: function (response) {
+        console.log(response); //TODO ARREGLO
+        
+        var template = $('#template-character').html(); //no cambia Imprime el template que escribimos en el html
+        var $characters = $('#characters-container'); //Objeto jquerisado ya tiene función append
+        // console.log(template);
+        // //console.log(response.data.results); 20 OBJETOS CON INFO
+        response._embedded.events.forEach(function (character) {
+
+
+          var data = {
+            name: character.name,
+            img: character.images[0].url,
+            url: character.url,
+          };
+          //   //console.log(character); //CADA UNO DE LOS 20 OBJETOS CON SUS ELEMENTOS
+          //   //console.log(data); //cambia cada ciclo
+          var filledTemplate = fillTemplate(template, data); //Combina nuestro template con los datos
+          //   console.log(filledTemplate);
+          $characters.append(filledTemplate); //
+        });
+      }
+    });
+  });
+  //SI QUEREMOS QUE SE EJECUTE AL HACER CLICK PODEMOS METER TODO DENTRO DE UN CLICK
+});
+
+//FUNCIONES QUE USAMOS PARA LAS PLANTILLAS Y PODER IMPRIMIR LA INFORMACIÓN
+//PLANTILLA
+function fillTemplate(template, data) {
+  for (var index in data) {
+    var value = data[index];
+    template = template.replace(new RegExp('{{' + index + '}}', 'g'), escapeHtml(value));
+  };
+  return template;
+}
+//FUNCIÓN QUE SALVA A ELEMENTOS Y SIMBOLOS
+function escapeHtml(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+
+// funcion guardar post
+function getText(event) {
+  event.preventDefault();
+  var valueTextArea = $(".post-text").val();
+  console.log(valueTextArea);
+  addComment(valueTextArea);
+
+
+  $('.post-text').val('');
+}
+
+var template = '<div class="row">' +
+  '<div class="col">' +
+  // '<textarea class="post-text"></textarea>' +
+  '<div class="col">' +
+  // '<button type="button" class="publish">Post</button>' +
+  '<p>_comment_<p>'
+'</div>' +
+'</div>' +
+'</div>'
+
+
+function addComment(valueTextArea) {
+  var finalTemplate = '';
+  finalTemplate = template.replace('_comment_', valueTextArea);
+  $(".post").append(finalTemplate);
+}
